9e9bbf77a71bd1709e517b114cc7cc28824c57ce
ymStudyLog <ylee585@gmail.com>
2022-05-27 16:05:42 +0900

    temp-commit-headerwrapper missing

--- src/Components/common/HeaderWrapper.js
+++ src/Components/common/HeaderWrapper.js
@@ -13,7 +13,6 @@ const StyledHeader = styled.div`
   justify-content: center;
   align-items: center;
 `;
-const HeaderWrapper = (props) => <StyledHeader {...props} />;
 
 /* headerWrapper안에 들어갈 element가 배치될 div */
 const BoxInsideStyledHeader = styled.div`
@@ -35,4 +34,5 @@ const EmptySpace = styled.div`
 `;
 export const Spacer = (props) => <EmptySpace {...props} />;
 
+const HeaderWrapper = (props) => <StyledHeader {...props} />;
 export default HeaderWrapper;
